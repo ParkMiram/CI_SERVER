ce36fba6cc8b7e200f81b7fa351aad38ec96f8d4
박미람 <myurm@a-dev.co.kr>
2024-05-24 02:17:57 +0900

    applicationTest 수정

--- src/test/java/com/example/ci/CiApplicationTests.java
+++ src/test/java/com/example/ci/CiApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.ci;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CiApplicationTests {
 
+	@Value("${spring.datasource.username}")
+	String username;
+
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void dbUsernameIsRoot(){
+		Assertions.assertEquals("root",username);
+	}
 }
